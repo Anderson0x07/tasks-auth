4b9de9b892d52cee5bdaa814eee710bb38816e77
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-07-05 18:05:34 -0500

    The logged in user context and validations are performed in the registration form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import { AuthProvider } from './context/AuthContext'
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<h1 className='text-4xl font-bold text-center'>Hola mundo</h1>} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/register' element={<RegisterPage/>} />
-        <Route path='/tasks' element={<h1 className='text-4xl font-bold text-center'>Tasks List</h1>} />
-        <Route path='/add-task' element={<h1 className='text-4xl font-bold text-center'>Add task</h1>} />
-        <Route path='/tasks/:id' element={<h1 className='text-4xl font-bold text-center'>Update Task</h1>} />
-        <Route path='/profile' element={<h1 className='text-4xl font-bold text-center'>Profile</h1>} />
-      </Routes>
-      
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<h1 className='text-4xl font-bold text-center'>Hola mundo</h1>} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='/tasks' element={<h1 className='text-4xl font-bold text-center'>Tasks List</h1>} />
+          <Route path='/add-task' element={<h1 className='text-4xl font-bold text-center'>Add task</h1>} />
+          <Route path='/tasks/:id' element={<h1 className='text-4xl font-bold text-center'>Update Task</h1>} />
+          <Route path='/profile' element={<h1 className='text-4xl font-bold text-center'>Profile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,27 +1,54 @@
 import { useForm } from 'react-hook-form'
-import { registerRequest } from '../api/auth'
+import { useAuth } from '../context/AuthContext'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function RegisterPage() {
 
-    const { register, handleSubmit } = useForm()
+    const { register, handleSubmit, formState:{
+        errors
+    } } = useForm()
 
-    const onSubmit = handleSubmit(async (values) => {
-        const res = await registerRequest(values)
-        console.log(res)
+    const { signUp, isAuthenticated, errors: registerErrors } = useAuth();
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(isAuthenticated) navigate('/tasks')
+    }, [isAuthenticated])
+
+    const onSubmit = handleSubmit((values) => {
+        signUp(values)
     })
 
     return (
         <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
+            {
+                registerErrors.map((err, i) => (
+                    <div key={i} className='bg-red-500 p-2 text-white'>
+                        {err}
+                    </div>
+                ))
+            }
             <form onSubmit={onSubmit}>
                 <input type="text" name="username" className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
                     placeholder='Username' {...register("username", { required: true })}
                 />
+                {
+                    errors.username && <p className='text-red-500'>Username is required</p>
+                }
                 <input type="email" name="email" className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
                     placeholder='Email' {...register("email", { required: true })}
                 />
+                {
+                    errors.email && <p className='text-red-500'>Email is required</p>
+                }
                 <input type="password" name="password" className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2'
                     placeholder='Password' {...register("password", { required: true })}
                 />
+                {
+                    errors.password && <p className='text-red-500'>Password is required</p>
+                }
 
                 <button type='submit'>Register</button>
             </form>
