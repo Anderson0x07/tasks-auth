865dc613dbc61e391773cbd77bec33ffbf449237
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-07-05 17:30:40 -0500

    CORS is configured, react form handling is configured, and user registration functionality is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 
 
 function App() {
@@ -6,8 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<h1 className='text-4xl font-bold text-center'>Hola mundo</h1>} />
-        <Route path='/login' element={<h1 className='text-4xl font-bold text-center'>Login</h1>} />
-        <Route path='/register' element={<h1 className='text-4xl font-bold text-center'>Register</h1>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/register' element={<RegisterPage/>} />
         <Route path='/tasks' element={<h1 className='text-4xl font-bold text-center'>Tasks List</h1>} />
         <Route path='/add-task' element={<h1 className='text-4xl font-bold text-center'>Add task</h1>} />
         <Route path='/tasks/:id' element={<h1 className='text-4xl font-bold text-center'>Update Task</h1>} />
