57198e963c717f8ce2ebbf64ce32a0050611d595
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-07-06 17:36:32 -0500

    DB's password added

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { PASSWORD } from "./var.js";
+//import { PASSWORD } from "./var.js";
 
 const DATABASE = 'merndb'
 const MONGODB_URI = `mongodb+srv://Anderson0x07:anderson0x07@clustertest.xzrg9bm.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
