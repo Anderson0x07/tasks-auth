d314e877a107df6a064f5419d79efe1aec985915
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-07-06 19:26:12 -0500

    Pass changed

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-//import { PASSWORD } from "./var.js";
 
 const DATABASE = 'merndb'
-const MONGODB_URI = `mongodb+srv://Anderson0x07:anderson0x07@clustertest.xzrg9bm.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
+const password = process.env.PASSWORD;
+const MONGODB_URI = `mongodb+srv://Anderson0x07:${password}@clustertest.xzrg9bm.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
 
 export const connectDB = async () => {
   try {
