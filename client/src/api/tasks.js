b0dffd01cd4f5a85e2957611a718e1d7554438f8
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-07-06 22:07:52 -0500

    Token into headers

--- client/src/api/tasks.js
+++ client/src/api/tasks.js
@@ -1,12 +1,15 @@
+import Cookies from "js-cookie"
 import axios from "./axios"
 
-export const getTasksRequest = () => axios.get(`/tasks`)
+const cookies = Cookies.get()
 
-export const getTaskRequest = (id) => axios.get(`/tasks/${id}`)
+export const getTasksRequest = () => axios.get(`/tasks`, { headers: { Authorization: `Bearer ${cookies.token}` } })
 
-export const createTaskRequest = (task) => axios.post(`/tasks`, task)
+export const getTaskRequest = (id) => axios.get(`/tasks/${id}`, { headers: { Authorization: `Bearer ${cookies.token}` } })
 
-export const updateTaskRequest = (id, task) => axios.put(`/tasks/${id}`, task)
+export const createTaskRequest = (task) => axios.post(`/tasks`, task, { headers: { Authorization: `Bearer ${cookies.token}` } })
 
-export const deleteTasksRequest = (id) => axios.delete(`/tasks/${id}`)
+export const updateTaskRequest = (id, task) => axios.put(`/tasks/${id}`, task, { headers: { Authorization: `Bearer ${cookies.token}` } })
+
+export const deleteTasksRequest = (id) => axios.delete(`/tasks/${id}`, { headers: { Authorization: `Bearer ${cookies.token}` } })
 
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,8 +3,8 @@ import { TOKEN_SECRET } from "../config.js";
 
 export const authRequired = (req, res, next) => {
     console.log("AUTH REQUIRED")
-    console.log(req.cookies)
-    const { token } = req.cookies;
+    console.log(req.headers.authorization.split(' ')[1])
+    const token = req.headers.authorization.split(' ')[1]
     console.log(token)
 
     if (!token) return res.status(401).json({ message: 'No token, authorization denied' })
